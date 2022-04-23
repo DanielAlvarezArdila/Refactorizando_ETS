2ba2ef1d32aec71e0a9796706d62194be22b8606
Daniel Alvarez <dan.alv.ard97@gmail.com>
2022-04-23 18:01:28 +0100

    se agrega la clase Idiomas

---
+++ inclui.edadfecha-master/Inclui/Inclui.EdadFecha/CSIdioma.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inclui.edadfecha.Inclui.Inclui.EdadFecha
+{
+    public class CSIdiomas
+    {
+        public static string[] ReadDataFromFile(string fileName)
+        {
+            string[] fileLines = new string[0];
+            try
+            {
+                StreamReader inputFile = new StreamReader(fileName);
+                while (!inputFile.EndOfStream)
+                {
+                    Array.Resize(ref fileLines, fileLines.Length + 1);
+                    fileLines[fileLines.Length - 1] = inputFile.ReadLine();
+                }
+                inputFile.Close();
+                if (fileLines.Length == 0)
+                {
+                    Console.WriteLine("Fichero vacío. Debe tener información para poder usarlo.");
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+                Array.Resize(ref fileLines, 0);
+            }
+            return (fileLines);
+        }
+    }
+}
